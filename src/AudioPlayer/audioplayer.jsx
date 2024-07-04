39f9db23caa948330f48140599b935573d72b94a
imran pasha <142788077+powerstone666@users.noreply.github.com>
2024-07-05 01:38:42 +0530

    Update audioplayer.jsx

--- src/AudioPlayer/audioplayer.jsx
+++ src/AudioPlayer/audioplayer.jsx
@@ -46,8 +46,10 @@ function AudioPlayerComponent() {
       setArray(res.data.data[0].album.name);
       setImage(res.data.data[0].image[1].url);
       const decodedName = he.decode(res.data.data[0].name);
-        localStorage.setItem('spotify',decodedName);
+       if(decodedName){ 
+localStorage.setItem('spotify',decodedName);
         setSpotify(decodedName);
+}
       setNames(decodedName);
       const url = res.data.data[0].downloadUrl[4].url;
       setMusic(url);
