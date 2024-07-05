ca5ba59f83e8b7f8dc97e1a4c1b4b1d0d995a6a4
imran pasha <imranpasha8225@gmail.com>
2024-07-05 17:47:28 +0530

    checking notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import React, { useContext, useEffect,useState } from "react";
+import React, { useContext, useEffect } from "react";
 import Sidebar from "./navbar/sidebar";
 import { Context } from "./main";
 import { useNavigate, useLocation } from 'react-router-dom';
 import Landing from "./landing"
 import "./App.css";
-import OneSignal from 'react-onesignal';
-
 function App() {
   const { setSelected } = useContext(Context);
   const navigate = useNavigate();
   const location = useLocation();
-
-  //Example2
-  const [initialized, setInitialized] = useState(false);
-  OneSignal.init({ appId: 'd6a22656-474f-40ac-9aec-82e657ee310e' }).then(() => {
-    setInitialized(true);
-    OneSignal.Slidedown.promptPush();
-    // do other stuff
-  })
   useEffect(() => {
     // Initialize 'selected' state from localStorage on first mount
     const lastSelected = localStorage.getItem("selected") || "/";
