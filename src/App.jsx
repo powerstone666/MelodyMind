0512dda5ea74fe1f4ee8855eff58ee61c941b644
imran pasha <imranpasha8225@gmail.com>
2024-07-05 16:56:27 +0530

    checking notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect } from "react";
+import addNotification from 'react-push-notification';
+import { Notifications } from 'react-push-notification';
 import Sidebar from "./navbar/sidebar";
 import { Context } from "./main";
 import { useNavigate, useLocation } from 'react-router-dom';
@@ -8,6 +10,22 @@ function App() {
   const { setSelected } = useContext(Context);
   const navigate = useNavigate();
   const location = useLocation();
+  useEffect(() => {
+    const notificationInterval = setInterval(() => {
+      addNotification({
+        title: 'Updates Under Way',
+        subtitle: 'updates under way',
+        message: 'Recommendations are under optimisation u may miss spotify recommendation for a day till that u can enjoy our v1',
+        theme: 'darkblue',
+        native: true // when using native, your OS will handle theming.
+    });
+    }, 1*60*60*1000); // Send notification every 5 seconds (5000 milliseconds)
+
+    return () => clearInterval(notificationInterval);
+  }, [])
+
+
+
   useEffect(() => {
     // Initialize 'selected' state from localStorage on first mount
     const lastSelected = localStorage.getItem("selected") || "/";
